5cbd3b03325d9a1272672a23373c16b07227753d
Nilomiranda <nilomiranda3@gmail.com>
2021-06-26 00:52:05 -0300

    setup relay compiler and runtime environment

---
+++ src/fetchGraphQL.ts
@@ -0,0 +1,18 @@
+async function fetchGraphQL(text, variables) {
+  // Fetch data from GitHub's GraphQL API:
+  const response = await fetch(process.env.API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: text,
+      variables,
+    }),
+  })
+
+  // Get the response as JSON
+  return response.json()
+}
+
+export default fetchGraphQL
